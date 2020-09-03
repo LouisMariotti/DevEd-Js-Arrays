b7aefa9de2b701df672d824b6710c08218e067d7
LouisMariotti <68160210+LouisMariotti@users.noreply.github.com>
2020-09-03 13:26:17 +0200

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,21 @@
+const listOfUser = ["DevEd", "Louis", "Ange"];
+// Array makes us do that instead of:
+const user1 = "Louis";
+const user2 = "DevEd";
+const user3 = "Ange";
+
+// To access it we do
+
+// console.log(listOfUser); // List of user is saw as an object
+
+// this const with array is saw as a function
+
+/* listOfUser.pop(); // Will delet the last element
+console.log(listOfUser); */
+
+/* listOfUser.push("NEW");
+console.log(listOfUser); // Will add new to the array */
+
+// console.log(listOfUser.length); // Will tell us how many users we have
+
+console.log(listOfUser.indexOf("Louis")); // Give the position
